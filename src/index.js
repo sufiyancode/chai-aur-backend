6b405eef82118edb8b1d7439a42346fe569e2d77
Shaikh Sufiyan <sksufiyan9860@gmail.com>
2024-09-17 12:52:31 +0530

    Api Error and Api Response handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config();
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runnig at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed !!!", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn) =>{ () =>{}}
+// function ke andar function pass kar rhe hai
+
+// next -> is used if the middleware is user
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
